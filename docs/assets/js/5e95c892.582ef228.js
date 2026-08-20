dd77afd0c342cdaa445823f49a65b66c2cefb8f1
Kuucheen <70746714+Kuucheen@users.noreply.github.com>
2026-08-20 15:16:24 +0200

    Deploy documentation

---
+++ docs/assets/js/5e95c892.582ef228.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmagpie_docs=globalThis.webpackChunkmagpie_docs||[]).push([[647],{7121(e,s,a){a.r(s),a.d(s,{default:()=>d});a(6540);var r=a(4164),c=a(7559),u=a(5500),i=a(2831),l=a(1656),o=a(4848);function d(e){return(0,o.jsx)(u.e3,{className:(0,r.A)(c.G.wrapper.docsPages),children:(0,o.jsx)(l.A,{children:(0,i.v)(e.route.routes)})})}}}]);
